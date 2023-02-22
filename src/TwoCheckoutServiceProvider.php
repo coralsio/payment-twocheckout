2ef9661cbb8bc30fe64b80ff3bbc0e4d4a9d2cb6
Enas <enasasmar882@gmail.com>
2023-02-22 12:21:46 +0200

    fixes service provider

--- src/TwoCheckoutServiceProvider.php
+++ src/TwoCheckoutServiceProvider.php
@@ -2,21 +2,27 @@
 
 namespace Corals\Modules\Payment\TwoCheckout;
 
-use Illuminate\Support\ServiceProvider;
+use Corals\Foundation\Providers\BasePackageServiceProvider;
 use Corals\Settings\Facades\Modules;
 
-class TwoCheckoutServiceProvider extends ServiceProvider
+class TwoCheckoutServiceProvider extends BasePackageServiceProvider
 {
+    /**
+     * @var
+     */
     protected $defer = false;
+    /**
+     * @var
+     */
+    protected $packageCode = 'corals-payment-twocheckout';
 
     /**
      * Bootstrap the application events.
      *
      * @return void
      */
-    public function boot()
+    public function bootPackage()
     {
-        $this->registerModulesPackages();
     }
 
     /**
@@ -24,7 +30,7 @@ class TwoCheckoutServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function registerPackage()
     {
     }
 
